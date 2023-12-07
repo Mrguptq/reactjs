c6f9ec85dd3ddb8762beed5cd14f23ba777021f4
Gupta <101403239+Mrguptq@users.noreply.github.com>
2023-12-07 11:48:11 +0530

    To-Do List Project using HTML & CSS & Javascript
    Small project using HTML CSS and JavaScript ...

---
+++ To-Do list Project/to-do.js
@@ -0,0 +1,47 @@
+let todoList = [
+  { 
+    item: 'Buy Milk', 
+    dueDate: '4/10/2023' },
+
+  { 
+    item: 'Go to College', 
+    dueDate: '4/12/2023'},
+];
+
+displayItems();
+
+function addTodo() {
+  let inputElement = document.querySelector("#todo-input");
+
+  let dateElements = document.querySelector("#todo_date");
+
+  let todoItem = inputElement.value;
+
+  let todoDate = dateElements.value;
+
+  todoList.push({item: todoItem, dueDate: todoDate});
+
+  inputElement.value = " ";
+
+  dateElements.value = " ";
+
+  displayItems();
+}
+
+function displayItems() {
+  let containerElement = document.querySelector(".todo_container");
+
+  let newHtml = "";
+
+  containerElement.innerText = " ";
+  for (let i = 0; i < todoList.length; i++) {
+    let {item, dueDate} = todoList[i];
+    newHtml += `
+        <span>${item}</span>
+         <span>${dueDate}</span>
+        <button class ="btn-delete" onclick="todoList.splice(${i}, 1);
+        displayItems();">Delete</button>
+        `;
+  }
+  containerElement.innerHTML = newHtml;
+}
